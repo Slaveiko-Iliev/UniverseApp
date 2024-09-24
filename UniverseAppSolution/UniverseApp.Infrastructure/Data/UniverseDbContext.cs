bf235cfe88f8d40ecbbe3f491ee6a0b01f0eedc8
Slaveiko-Iliev <130203252+Slaveiko-Iliev@users.noreply.github.com>
2024-09-24 06:18:58 +0300

    Add project files.

---
+++ UniverseAppSolution/UniverseApp.Infrastructure/Data/UniverseDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UniverseApp.Infrastructure.Data
+{
+    public class UniverseDbContext : IdentityDbContext
+    {
+        public UniverseDbContext(DbContextOptions<UniverseDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
